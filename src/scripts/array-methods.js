f11827e2f0894aa07eca62f837c4d151f06cdb4f
Sam Alpren <samalpren1@gmail.com>
2019-03-20 12:16:00 -0500

    first exercise of solar system

---
+++ src/scripts/array-methods.js
@@ -0,0 +1,35 @@
+const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+
+console.log(planets)
+
+/*
+    Use the forEach method to add the name of each planet
+    to a section element in your HTML with an id of "planets".
+    Use string templates to construct the DOM elements.
+*/
+
+const planetEl = document.getElementById("planets")
+
+planets.forEach(planet => {
+  planetEl.innerHTML += `
+     <section>${planet}</section>`
+     planetEl.innerHTML += "<hr/>"
+});
+
+
+/*
+    Use the map method to create a new array where the
+    first letter of each planet is capitalized. Use the
+    `toUpperCase()` method on strings.
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+*/
+
+
+/*
+    Use the filter method to create a new array that
+    contains planets with the letter 'e'. Use the `includes()`
+    method on strings.
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+*/
